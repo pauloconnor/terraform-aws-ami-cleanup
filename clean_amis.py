4a4aaf62e04a283e9dc8187d7f958fad15e7e8c8
Paul O'Connor <465717+pauloconnor@users.noreply.github.com>
2021-12-08 18:04:21 +0000

    Update script to actually work

--- clean_amis.py
+++ clean_amis.py
@@ -5,25 +5,26 @@ import os
 
 ACCOUNT_ID = os.environ["ACCOUNT_ID"]
 DELETE_OLDER_THAN_DAYS = os.environ["DELETE_OLDER_THAN_DAYS"]
-EXCLUSION_TAG = os.environ["EXCLUSION_TAG"]
-TAG_FILTER = os.environ["TAG_FILTER"]
+EXCLUSION_TAG = os.getenv("EXCLUSION_TAG", "DONOTDELETE")
 
 
 def lambda_handler(event, context):
-    ec2 = boto3.ec2("ec2")
-    response = ec2.describe_images(
-        Filters=[
-            {"Name": f"tag:{TAG_FILTER}", "Values": ["*"]},
-            {"Name": "Owners", "Values": [ACCOUNT_ID]},
-        ]
-    )
-    print("Total images with this tag " + str(len(response["Images"])))
+    ec2 = boto3.client("ec2")
+    response = ec2.describe_images(Owners=[ACCOUNT_ID])
+    print("Total images " + str(len(response["Images"])))
 
-    used_images = {instance.image_id for instance in ec2.instances.all()}
+    used_images = []
+    existing_instances = ec2.describe_instances()
+    for reservations in existing_instances["Reservations"]:
+        for instances in reservations["Instances"]:
+            if instances["ImageId"] not in used_images:
+                used_images.append(instances["ImageId"])
+    print(used_images)
 
     deleted_images = 0
     for image in response["Images"]:
-        if image in used_images:
+        if image["ImageId"] in used_images:
+            print(image["ImageId"] + "is in use")
             continue
 
         create_date = parser.parse(image["CreationDate"])
@@ -32,11 +33,23 @@ def lambda_handler(event, context):
         ) - create_date.replace(tzinfo=None)
 
         if time_between_creation.days > int(DELETE_OLDER_THAN_DAYS):
-            for tag in image["Tags"]:
-                if tag["Key"] == EXCLUSION_TAG and tag["Value"] != "True":
+            if "Tags" in image:
+                delete = True
+                for tag in image["Tags"]:
+                    if tag["Key"] == EXCLUSION_TAG:
+                        delete = False
+                if delete:
                     deleted_images += 1
-                    print("Deleting " + image["ImageId"] + " " + image["Name"])
-                    _ = ec2.deregister_image(ImageId=image["ImageId"])
+                    print(
+                        "Deleting %s %s, Created %s"
+                        % (
+                            image["ImageId"],
+                            image["Name"],
+                            image["CreationDate"],
+                        )  # noqa: E501
+                    )
+
+                    # _ = ec2.deregister_image(ImageId=image["ImageId"])
 
     print("Number of images deleted " + str(deleted_images))
     return True
